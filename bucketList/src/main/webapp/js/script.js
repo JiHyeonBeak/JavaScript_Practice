2ac95eb2e27c1369adcf2022fa50e493c2c2369d
JiHyeonBeak <jadehl09@gmail.com>
2021-08-20 17:05:33 +0900

    fix : Bucket List

--- bucketList/src/main/webapp/js/script.js
+++ bucketList/src/main/webapp/js/script.js
@@ -1,12 +1,20 @@
 
-
+	var count = 0;
 function addlist() {
 	var thing = document.getElementById('addtext').value;
 	var li = document.createElement('li');
+	console.log(count);
+	if(count == 12) {
+		alert("리스트 창이 다 찼습니다. 이제 버킷리스트를 이루어 보세요!")
+		document.getElementById('addtext').setAttribute('class','active');
+		document.getElementById('ok').setAttribute('class','active');
+	}
+	else {
 	li.innerHTML = '<li>'+thing+'</li>';
 	document.getElementById('list').appendChild(li);
 	document.getElementById('addtext').value='';
-
+	count++;
+	}
 	li.addEventListener('click', function() {
 		li.classList.toggle("done");
 	})
